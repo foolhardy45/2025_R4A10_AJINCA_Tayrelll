8f4aa71029d05fe90d83224717771eb246d77289
foolhardy45 <ajincatayrell@gmail.com>
2025-02-11 16:53:20 +0100

    Add hero and carousel components with associated styles and assets

--- angular_tp2/src/app/app.component.ts
+++ angular_tp2/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {OurServiceComponent} from "./our-service/our-service.component";
+import {CarouselComponent} from "./carousel/carousel.component";
+import {HeroComponent} from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, OurServiceComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, OurServiceComponent, CarouselComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
