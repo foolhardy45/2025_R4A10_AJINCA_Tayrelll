58e09f7626d4057124cd0466ae4d7f678fe3ef76
foolhardy45 <ajincatayrell@gmail.com>
2025-03-07 18:26:21 +0100

    Refactor book list component to manage subscriptions and clean up on destroy

--- TP4/src/app/pages/book-list-page/book-list-page.component.ts
+++ TP4/src/app/pages/book-list-page/book-list-page.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Book } from '../../models/book';
 import { BooksInMemoryService } from '../../services/book-inmemory.service';
 import { RouterLink } from '@angular/router';
-import {Observable} from "rxjs";
+import { Subscription} from "rxjs";
 
 
 @Component({
@@ -14,16 +14,19 @@ import {Observable} from "rxjs";
 })
 export class BookListPageComponent implements OnInit {
   private readonly bookService = inject(BooksInMemoryService);
-  books$!: Observable<Book[]>;
+  private subscription = new Subscription();
   book: Book[] = [];
 
 
 
   ngOnInit() {
-    this.books$ = this.bookService.getAllBooks();
-    this.bookService.getAllBooks().subscribe((data: Book[]) => {
-      this.book = data;
-    })
+    this.subscription = this.bookService.getAllBooks().subscribe((data) => {
+        this.book = data;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
